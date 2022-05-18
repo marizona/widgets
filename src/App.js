fb91d59bb3e78f1c33a2f01e66e91b45f62a5bc5
marizona <marie.jacquier@epitech.eu>
2022-05-18 17:20:17 +0200

    term search

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Accordion from "./components/Accordion";
+import Search from "./components/Search";
 
 const items = [
   {
@@ -19,7 +20,8 @@ const items = [
 function App() {
   return (
     <div>
-      <Accordion items={items}/>
+      <Search/>
+      {/* <Accordion items={items}/> */}
     </div>
   );
 }
---
+++ src/components/Search.js
@@ -0,0 +1,25 @@
+import React, { useEffect, useState } from "react";
+
+function Search() {
+  const [term, setTerm] = useState("");
+  useEffect(() => {
+    
+  }, [term]);
+
+  return (
+    <div>
+      <div className="ui form">
+        <div className="field">
+          <label>Enter Search Term</label>
+          <input
+            value={term}
+            onChange={(e) => setTerm(e.target.value)}
+            className="input"
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Search;
