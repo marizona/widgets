394f61e99f0e5525dc026e9134718d09dd6cc0fd
marizona <marie.jacquier@epitech.eu>
2022-05-19 11:22:22 +0200

    seach dynamic done

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,14 +1,49 @@
+import axios from "axios";
 import React, { useEffect, useState } from "react";
 
 function Search() {
-  const [term, setTerm] = useState("");
+  const [term, setTerm] = useState("programming");
+  const [results, setResults] = useState([]);
+  
+
   useEffect(() => {
+    const search = async () => {
+      const {data} = await axios.get("https://en.wikipedia.org/w/api.php", {
+        params: {
+          action: "query",
+          list: "search",
+          origin: "*",
+          format: "json",
+          srsearch: term,
+        },
+      });
+      setResults(data.query.search);
+    };
+if(term){
+search();
+}
     
   }, [term]);
 
+  const renderedResults = results.map((result) => {
+    return (
+      <div key={result.pageid} className="item">
+        <div className="content">
+          <a
+            href={`https://en.wikipedia.org?curid=${result.pageid}`}
+            className="header"
+          >
+            {result.title}
+          </a>
+          <span dangerouslySetInnerHTML={{ __html: result.snippet }}></span>
+        </div>
+      </div>
+    );
+  });
+
   return (
     <div>
-      <div className="ui form 3">
+      <div className="ui form">
         <div className="field">
           <label>Enter Search Term</label>
           <input
@@ -18,6 +53,7 @@ function Search() {
           />
         </div>
       </div>
+      <div className="ui celled list">{renderedResults}</div>
     </div>
   );
 }
