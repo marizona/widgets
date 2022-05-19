1a7eab7069511c516e493e4825901b662387eab2
marizona <marie.jacquier@epitech.eu>
2022-05-19 14:31:29 +0200

    debounced

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, debouncedTerm } from "react";
 
 function Search() {
   const [term, setTerm] = useState("programming");
@@ -33,6 +33,25 @@ function Search() {
     }
   }, [term]);
 
+    useEffect(() => {
+      const search = async () => {
+        const { data } = await axios.get("https://en.wikipedia.org/w/api.php", {
+          params: {
+            action: "query",
+            list: "search",
+            origin: "*",
+            format: "json",
+            srsearch: debouncedTerm,
+          },
+        });
+
+        setResults(data.query.search);
+      };
+      if (debouncedTerm) {
+        search();
+      }
+    }, [debouncedTerm]);
+
   const renderedResults = results.map((result) => {
     return (
       <div key={result.pageid} className="item">
@@ -47,7 +66,7 @@ function Search() {
         </div>
       </div>
     );
-  });
+  }); 
 
   return (
     <div>
