72d591fb70c03759d32d8194eed61778a980a0ee
marizona <marie.jacquier@epitech.eu>
2022-05-23 15:05:54 +0200

    issue sorted cors

--- src/components/Convert.js
+++ src/components/Convert.js
@@ -5,12 +5,12 @@ const Convert = ({ language, text }) => {
   useEffect(() => {
     axios.post(
       "https://translation.googleapis.com/language/translate/v2",
-      {},
+      undefined,
       {
         params: {
           q: text,
+          key: "AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM",
           target: language.value,
-          key: "AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM"
         },
       }
     );
